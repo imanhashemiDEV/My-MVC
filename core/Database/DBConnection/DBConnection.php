2294901b09259ea1d325123a1055fa3b662dd915
Niman-Co <hashemi.iman@gmail.com>
2023-06-10 17:09:12 +0330

    find

--- core/Database/DBConnection/DBConnection.php
+++ core/Database/DBConnection/DBConnection.php
@@ -37,4 +37,9 @@ class DBConnection
             return false;
         }
     }
+
+    public static function newInsertedId()
+    {
+        return self::getDBConnectionInstance()->lastInsertId();
+    }
 }
--- core/Database/Traits/HasCRUD.php
+++ core/Database/Traits/HasCRUD.php
@@ -24,6 +24,18 @@ trait HasCRUD{
         $this->setSql("INSERT INTO {$this->table} SET ". $this->setFillables() . $this->createdAt."=Now();");
         $this->executeQuery();
         $this->resetQuery();
+        // name age default
+        // is_admin status
+        $object = $this->find(DBConnection::newInsertedId());
+        $defaultVariables = get_class_vars(get_called_class());
+        $allVariables = get_object_vars($object);
+         $differentVariables = array_diff(array_keys($allVariables),array_keys($defaultVariables));
+         foreach ($differentVariables as $attribute){
+             $this->$attribute = $object->$attribute;
+         }
+        $this->resetQuery();
+         return $this;
+
     }
 
     protected function update(){
@@ -32,6 +44,20 @@ trait HasCRUD{
         $this->setValue($this->primaryKey, $this->{$this->primaryKey});
         $this->executeQuery();
         $this->resetQuery();
+        return $this;
+    }
+
+    protected function find($id){
+        $this->setSql("SELECT * FROM ".$this->table);
+        $this->setWhere("AND" , $this->primaryKey . " = ? ");
+        $this->setValue($this->primaryKey, $id);
+        $statement = $this->executeQuery();
+        $data = $statement->fetch();
+        if($data){
+            return $this->setAttributes($data);
+        }else{
+            return null;
+        }
     }
 
 
