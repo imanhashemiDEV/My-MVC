f029c235da81106d2eb6ed89ed9583ae36b6ffea
Niman-Co <hashemi.iman@gmail.com>
2023-07-07 12:01:54 +0330

    'method-caller

---
+++ core/Database/Traits/HasMethodCaller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Core\Database\Traits;
+
+trait HasMethodCaller
+{
+//    User::where()->find();
+
+    private $methods = ['insert','update','find','get','delete','where','orderBy','limit'];
+
+    private function methodCaller($object,$method,$params){
+      return  call_user_func_array(array($object, $method), $params);
+    }
+}
